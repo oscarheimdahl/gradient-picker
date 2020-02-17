6e188e4e2852a43fe1d77484c569edd6b4f47b63
oscarheimdahl <o.heimdahl@gmail.com>
2020-02-17 16:50:57 +0100

    Updates

---
+++ precache-manifest.e0947bdf83d31472d4093efd2873457c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67df9b95f674ae8ee1655caaecf6af1e",
+    "url": "/gradient-picker/index.html"
+  },
+  {
+    "revision": "140070728f7d385734e3",
+    "url": "/gradient-picker/static/css/main.f1730144.chunk.css"
+  },
+  {
+    "revision": "961324b026d68de948ee",
+    "url": "/gradient-picker/static/js/2.b0762d8b.chunk.js"
+  },
+  {
+    "revision": "32c483d70943cd2bd83617a91103d810",
+    "url": "/gradient-picker/static/js/2.b0762d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140070728f7d385734e3",
+    "url": "/gradient-picker/static/js/main.f7fddd6b.chunk.js"
+  },
+  {
+    "revision": "d5feaaaf0917c23558ae",
+    "url": "/gradient-picker/static/js/runtime-main.cdeda3e1.js"
+  },
+  {
+    "revision": "19d98df802469d136279cc386d8b6e47",
+    "url": "/gradient-picker/static/media/hide.19d98df8.png"
+  }
+]);
